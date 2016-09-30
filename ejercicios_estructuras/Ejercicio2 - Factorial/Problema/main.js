df2ecf33bf7db1cf0a7cefb6bb848ee34ca0b570
adrianaVN <venegasadriana78@gmail.com>
2016-09-30 20:32:47 +0000

    estructuras repetitivas

---
+++ ejercicios_estructuras/Ejercicio2 - Factorial/Problema/main.js
@@ -0,0 +1,10 @@
+// Escribe aquí tu codigo Javascript
+
+
+
+var n = parseInt(prompt('ingrese numero'));
+var multiplicar=n
+for(var i=n-1; i>=1; i--){
+	(multiplicar = multiplicar*i);
+	console.log(multiplicar);
+}
